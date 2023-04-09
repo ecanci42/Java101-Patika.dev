e8e28b6ecf3cf3fc3ceac81f61dec17b6a069e0c
Emre <plonex95@gmail.com>
2023-04-09 23:39:06 +0300

    JAVA101

---
+++ Hipotenus.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Hipotenus
+{
+    public static void main(String[] args)
+    {
+        int a, b;
+        double c;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("1. Kenarı girin: ");
+        a = input.nextInt();
+
+        System.out.print("2. Kenarı girin: ");
+        b = input.nextInt();
+
+        c = Math.sqrt((a*a) + (b*b));
+
+        System.out.println("Hipotenüs: " + c);
+    }
+}
